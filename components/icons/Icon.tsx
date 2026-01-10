98154b9cab81fdb908787fd430ed4e38551a8d3a
unknown <charliz0828@gmail.com>
2026-01-11 00:06:29 +0800

    Fix Icon.tsx problem

--- components/icons/Icon.tsx
+++ components/icons/Icon.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { cn } from '@/lib/utils';
 import * as LucideIcons from 'lucide-react';
+import type { LucideProps } from 'lucide-react';
 
 export type IconName = keyof typeof LucideIcons;
 
-export interface IconProps extends React.SVGProps<SVGSVGElement> {
+export interface IconProps extends Omit<LucideProps, 'ref'> {
   name: IconName;
-  size?: number | string;
-  className?: string;
 }
 
 const Icon: React.FC<IconProps> = ({ name, size = 24, className, ...props }) => {
-  const LucideIcon = LucideIcons[name] as React.ComponentType<
-    React.SVGProps<SVGSVGElement>
-  >;
+  const LucideIcon = LucideIcons[name] as React.ComponentType<LucideProps>;
 
   if (!LucideIcon) {
     console.warn(`Icon "${name}" not found`);
